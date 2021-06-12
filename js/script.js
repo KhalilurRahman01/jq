e92e4edb2865c74a8391e05f7ad8f37c3db63cee
Khalilur Rahman <79338306+KhalilurRahman01@users.noreply.github.com>
2021-06-13 02:28:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,315 @@
+$(document).ready(function(){
+
+
+	//draggable
+
+	$( "#draggable" ).draggable();
+
+   //droppable
+
+	$( "#dragg" ).draggable();
+    $( "#dropp" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+
+   //selectable
+
+ $( "#selectable" ).selectable();
+
+ //resizable
+
+ $( "#resizable" ).resizable();
+
+//Sortable
+
+
+$( ".column" ).sortable({
+      connectWith: ".column",
+      handle: ".portlet-header",
+      cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all"
+    });
+ 
+    $( ".portlet" )
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".portlet-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+ 
+    $( ".portlet-toggle" ).on( "click", function() {
+      var icon = $( this );
+      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+      icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    });
+
+
+    //Widgets
+    //Datepicker
+
+        $( "#datepicker" ).datepicker();
+
+    //selectmenu
+    
+
+   var circle = $( "#circle" );
+ 
+    $( "#radius" ).selectmenu({
+      change: function( event, data ) {
+        circle.css({
+          width: data.item.value,
+          height: data.item.value
+        });
+      }
+     });
+ 
+    $( "#color" ).selectmenu({
+       change: function( event, data ) {
+         circle.css( "background", data.item.value );
+       }
+     });
+
+
+//progressbar
+
+    var progressbar = $( "#progressbar" ),
+      progressLabel = $( ".progress-label" );
+ 
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "Complete!" );
+      }
+    });
+ 
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+ 
+      progressbar.progressbar( "value", val + 2 );
+ 
+      if ( val < 99 ) {
+        setTimeout( progress, 80 );
+      }
+    }
+ 
+    setTimeout( progress, 2000 );
+
+
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+    //Effects
+    //Toggle
+
+    function runEffect() {
+      // get effect type from
+      var selectedEffect = $( "#effectTypes" ).val();
+ 
+      // Most effect types need no options passed by default
+      var options = {};
+      // some effects have required parameters
+      if ( selectedEffect === "scale" ) {
+        options = { percent: 50 };
+      } else if ( selectedEffect === "size" ) {
+        options = { to: { width: 200, height: 60 } };
+      }
+ 
+      // Run the effect
+      $( "#effect" ).toggle( selectedEffect, options, 500 );
+    };
+ 
+    // Set effect from select menu value
+    $( "#button1" ).on( "click", function() {
+      runEffect();
+    });
+//Color Animation
+
+
+     var state = true;
+    $( "#button2" ).on( "click", function() {
+      if ( state ) {
+        $( "#effect2" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect2" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+
+
+     });
+
+    $( "#button" ).on( "click", function() {
+      $( ".newClass" ).switchClass( "newClass", "anotherNewClass", 1000 );
+      $( ".anotherNewClass" ).switchClass( "anotherNewClass", "newClass", 1000 );
+    });
+
+    function position() {
+      $( ".positionable" ).position({
+        of: $( "#parent" ),
+        my: $( "#my_horizontal" ).val() + " " + $( "#my_vertical" ).val(),
+        at: $( "#at_horizontal" ).val() + " " + $( "#at_vertical" ).val(),
+        collision: $( "#collision_horizontal" ).val() + " " + $( "#collision_vertical" ).val()
+      });
+    }
+ 
+    $( ".positionable" ).css( "opacity", 0.5 );
+ 
+    $( "select, input" ).on( "click keyup change", position );
+ 
+    $( "#parent" ).draggable({
+      drag: position
+    });
+ 
+    position();
+//
+    // the widget definition, where "custom" is the namespace,
+    // "colorize" the widget name
+    $.widget( "custom.colorize", {
+      // default options
+      options: {
+        red: 255,
+        green: 0,
+        blue: 0,
+ 
+        // Callbacks
+        change: null,
+        random: null
+      },
+ 
+      // The constructor
+      _create: function() {
+        this.element
+          // add a class for theming
+          .addClass( "custom-colorize" );
+ 
+        this.changer = $( "<button>", {
+          text: "change",
+          "class": "custom-colorize-changer"
+        })
+        .appendTo( this.element )
+        .button();
+ 
+        // Bind click events on the changer button to the random method
+        this._on( this.changer, {
+          // _on won't call random when widget is disabled
+          click: "random"
+        });
+        this._refresh();
+      },
+ 
+      // Called when created, and later when changing options
+      _refresh: function() {
+        this.element.css( "background-color", "rgb(" +
+          this.options.red +"," +
+          this.options.green + "," +
+          this.options.blue + ")"
+        );
+ 
+        // Trigger a callback/event
+        this._trigger( "change" );
+      },
+ 
+      // A public method to change the color to a random value
+      // can be called directly via .colorize( "random" )
+      random: function( event ) {
+        var colors = {
+          red: Math.floor( Math.random() * 256 ),
+          green: Math.floor( Math.random() * 256 ),
+          blue: Math.floor( Math.random() * 256 )
+        };
+ 
+        // Trigger an event, check if it's canceled
+        if ( this._trigger( "random", event, colors ) !== false ) {
+          this.option( colors );
+        }
+      },
+ 
+      // Events bound via _on are removed automatically
+      // revert other modifications here
+      _destroy: function() {
+        // remove generated elements
+        this.changer.remove();
+ 
+        this.element
+          .removeClass( "custom-colorize" )
+          .enableSelection()
+          .css( "background-color", "transparent" );
+      },
+ 
+      // _setOptions is called with a hash of all options that are changing
+      // always refresh when changing options
+      _setOptions: function() {
+        // _super and _superApply handle keeping the right this-context
+        this._superApply( arguments );
+        this._refresh();
+      },
+ 
+      // _setOption is called for each individual option that is changing
+      _setOption: function( key, value ) {
+        // prevent invalid color values
+        if ( /red|green|blue/.test(key) && (value < 0 || value > 255) ) {
+          return;
+        }
+        this._super( key, value );
+      }
+    });
+ 
+    // Initialize with default options
+    $( "#my-widget1" ).colorize();
+ 
+    // Initialize with two customized options
+    $( "#my-widget2" ).colorize({
+      red: 60,
+      blue: 60
+    });
+ 
+    // Initialize with custom green value
+    // and a random callback to allow only colors with enough green
+    $( "#my-widget3" ).colorize( {
+      green: 128,
+      random: function( event, ui ) {
+        return ui.green > 128;
+      }
+    });
+ 
+    // Click to toggle enabled/disabled
+    $( "#disable" ).on( "click", function() {
+      // use the custom selector created for each widget to find all instances
+      // all instances are toggled together, so we can check the state from the first
+      if ( $( ":custom-colorize" ).colorize( "option", "disabled" ) ) {
+        $( ":custom-colorize" ).colorize( "enable" );
+      } else {
+        $( ":custom-colorize" ).colorize( "disable" );
+      }
+    });
+ 
+    // Click to set options after initialization
+    $( "#green" ).on( "click", function() {
+      $( ":custom-colorize" ).colorize( "option", {
+        red: 64,
+        green: 250,
+        blue: 8
+      });
+    });
+
+ 
+
+
+
+
+
+});
+
